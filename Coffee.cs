834ef70ff42cc6975cb8a9f1989d4eb933cd84dd
Petko <limona.pb@gmail.com>
2019-07-06 02:04:29 +0300

    Commit Restaurant Exercise Class

---
+++ Coffee.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMililiters = 50;
+
+        private const decimal CoffeePrice = 3.50m;
+
+        public Coffee(string name, double coffeine)
+            :base(name,CoffeePrice,CoffeeMililiters)
+        {
+            Caffeine = coffeine;
+        }
+        public double  Caffeine { get; set; }
+    }
+}
